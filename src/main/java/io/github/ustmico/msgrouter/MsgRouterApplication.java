4eb63e54dd67799f2562e58a26f5bf37328dbf18
Pascal Weiß <pa-weiss@gmx.de>
2019-05-07 12:16:57 +0200

    added basic function structure

---
+++ src/main/java/io/github/ustmico/msgrouter/MsgRouterApplication.java
@@ -0,0 +1,13 @@
+package io.github.ustmico.msgrouter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsgRouterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsgRouterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/ustmico/msgrouter/MsgRouterApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.ustmico.msgrouter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MsgRouterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
